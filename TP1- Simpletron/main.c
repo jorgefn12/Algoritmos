a0bdc5a83800cb5f4e2be8770775998da96755bb
Karen <31966611+oonosan@users.noreply.github.com>
2018-05-13 20:12:16 -0300

    Update main.c

--- TP1- Simpletron/main.c
+++ TP1- Simpletron/main.c
@@ -21,21 +21,31 @@
 #define CLA_IF "-if"
 #define CLA_IF_TXT "txt"
 #define CLA_IF_BIN "bin"
+#define CLA_O "-o"
+#define CLA_OF "-of"
 
 #define DEFAULT_M 50
 #define MAX_STR 200
+#define CANT_MAX_ARG 11
+#define CANT_MIN_ARG 2
 
 #define MSJ_MAS_AYUDA "Ingrese -h para mas ayuda"
 #define MSJ_ERROR_IF_NO_VALIDO "El ingreso de archivo de entrada es invalido"
 #define MSJ_ERROR_ARCHIVO_I_NO_INGRESADO "No se ingreso el archivo de entrada"
 #define MSJ_ERROR_IF_NO_INGRESADO "No se ingreso el tipo de archivo de entrada"
+#define MSJ_ERROR_OF_NO_VALIDO "El ingreso de archivo de salida es invalido"
+#define MSJ_ERROR_OF_NO_INGRESADO "No se ingreso el tipo de archivo de salida"
+#define MSJ_ERROR_CANT_ARG "La cantidad de argumentos ingresados no es valido"
 
 typedef enum {
     ST_OK,
+    ST_ERROR_CANT_ARG,
     ST_HELP,
     ST_ERROR_ARCHIVO_I_NO_INGRESADO,
     ST_ERROR_IF_NO_VALIDO,
-            ST_ERROR_IF_NO_INGRESADO
+    ST_ERROR_IF_NO_INGRESADO,
+    ST_ERROR_OF_NO_VALIDO,
+    ST_ERROR_OF_NO_INGRESADO
 } status_t;
 
 typedef enum {
@@ -49,27 +59,32 @@ typedef enum {
 } archivo_t;
 
 /*En esta función se validan los argumentos pasados por la terminal*/
-status_t validacion_cla(int argc, char **argv, int *m, char *archivo_i, archivo_t tipo_archivo);
+status_t validacion_cla(int argc, char **argv, int *m, char *archivo_i, archivo_t tipo_archivo_i, char *archivo_f, archivo_t tipo_archivo_f);
 /*En procesar_linea se obtiene cada linea del lms*/
 status_t procesar_linea(char *cadena);
 void imprimir_errores(status_t status);
 
 int main(int argc, char** argv) {
-
+    status_t status;
+    
+    printf("argc = %d\n", argc);
     return EXIT_SUCCESS;
 }
 
 /*Esta funcion valida los argumentos pasados por la linea de comandos
  *El ingreso de argumentos en este caso no deben tener un orden especifico*/
-status_t validacion_cla(int argc, char **argv, int *m, char *archivo_i, archivo_t tipo_archivo) {
+status_t validacion_cla(int argc, char **argv, int *m, char *archivo_i, archivo_t tipo_archivo_i, char *archivo_f, archivo_t tipo_archivo_f) {
     size_t i;
     char * p;
-    bool_t encontrado = FALSE;
+    bool_t encontrado = FALSE, encontrado_of = FALSE;
+
+    if (argc > CANT_MAX_ARG || argc<CANT_MIN_ARG)
+        return ST_ERROR_CANT_ARG;
 
     /*-----------------------------AYUDA-----------------------------
      * simpletron -h
      * Cuando se pide una ayuda solo se debe ingresar el comando de ayuda y nada mas*/
-    if (argc == 2 && (strcmp(argv[POS_ARGV1], CLA_AYUDA)) == 0)
+    if (argc == CANT_MIN_ARG && (strcmp(argv[POS_ARGV1], CLA_AYUDA)) == 0)
         return ST_HELP;
 
     /*-----------------------------MEMORIA-----------------------------
@@ -92,9 +107,9 @@ status_t validacion_cla(int argc, char **argv, int *m, char *archivo_i, archivo_
         /*Se busca el argumento "-i" en argv*/
         if ((strcmp(argv[i], CLA_I)) == 0) {
             if ((strcmp(argv[i + 1], CLA_I_DEFAULT)) == 0) {
-                tipo_archivo = ARCHIVO_DEFAULT;
+                tipo_archivo_i = ARCHIVO_DEFAULT;
             } else {
-                //Guardo el nombre del archivo
+                /*Guardo el nombre del archivo*/
                 memcpy(archivo_i, argv[i + 1], MAX_STR);
             }
             encontrado = TRUE;
@@ -108,15 +123,15 @@ status_t validacion_cla(int argc, char **argv, int *m, char *archivo_i, archivo_
 
 
     /*-----------------------------IF-----------------------------
-     * */
+     * Se obtiene si el tipo de archivo de entrada sera interpretado como bin o txt*/
     for (i = 0; i < argc; i++) {
         /*Se busca el argumento "-if" en argv*/
         if (strcmp(argv[i], CLA_IF) == 0) {
             /*Se busca el tipo de archivo de entrada: txt, bin*/
             if ((strcmp(argv[i + 1], CLA_IF_BIN)) == 0) {
-                tipo_archivo = ARCHIVO_BIN;
+                tipo_archivo_i = ARCHIVO_BIN;
             } else if ((strcmp(argv[i + 1], CLA_IF_TXT)) == 0) {
-                tipo_archivo = ARCHIVO_TXT;
+                tipo_archivo_i = ARCHIVO_TXT;
             } else
                 return ST_ERROR_IF_NO_VALIDO;
             encontrado = TRUE;
@@ -125,18 +140,69 @@ status_t validacion_cla(int argc, char **argv, int *m, char *archivo_i, archivo_
     }
 
     if (encontrado != TRUE)
-        /*Si no se encuentra el argumento "-i"*/
+        /*Si no se encuentra el argumento "-if"*/
         return ST_ERROR_IF_NO_INGRESADO;
+    encontrado = FALSE;
 
+    /*-----------------------------O-----------------------------
+     * No es obligatorio ingresar esta opcion */
+    for (i = 0; i < argc; i++) {
+        /*Se busca el argumento "-o" en argv*/
+        if ((strcmp(argv[i], CLA_O)) == 0) {
+            /*Copio el nombre del archivo de salida*/
+            memcpy(archivo_f, argv[i + 1], MAX_STR);
+            encontrado = TRUE;
+            i = argc;
+        }
+    }
+
+    /*Si no se encontro el argumento o se tomara como default stdin*/
+    if (encontrado != TRUE)
+        tipo_archivo_f = ARCHIVO_DEFAULT;
+
+
+    /*-----------------------------OF-----------------------------
+     * Se obtiene si el tipo de archivo de salida sera interpretado como bin o txt*/
+    if (encontrado == TRUE) { /*Si no se ingreso -o, no se debe ingresar -of*/
+        for (i = 0; i < argc; i++) {
+            /*Se busca el argumento "-of" en argv*/
+            if (strcmp(argv[i], CLA_OF) == 0) {
+                /*Se busca el tipo de archivo de entrada: txt, bin*/
+                if ((strcmp(argv[i + 1], CLA_IF_BIN)) == 0) {
+                    tipo_archivo_f = ARCHIVO_BIN;
+                } else if ((strcmp(argv[i + 1], CLA_IF_TXT)) == 0) {
+                    tipo_archivo_f = ARCHIVO_TXT;
+                } else
+                    return ST_ERROR_OF_NO_VALIDO;
+                encontrado_of = TRUE;
+                i = argc;
+            }
+        }
+    }
+
+    if (encontrado_of != TRUE)
+        /*Si no se encuentra el argumento "-of"*/
+        return ST_ERROR_OF_NO_INGRESADO;
+
+    return ST_OK;
 }
 
 void imprimir_errores(status_t status) {
+    if (status == ST_ERROR_CANT_ARG)
+        fprintf(stderr, "%s. %s\n", MSJ_ERROR_CANT_ARG, MSJ_MAS_AYUDA);
+    
     if (status == ST_ERROR_ARCHIVO_I_NO_INGRESADO)
         fprintf(stderr, "%s. %s\n", MSJ_ERROR_ARCHIVO_I_NO_INGRESADO, MSJ_MAS_AYUDA);
 
     if (status == ST_ERROR_IF_NO_VALIDO)
         fprintf(stderr, "%s. %s\n", MSJ_ERROR_IF_NO_VALIDO, MSJ_MAS_AYUDA);
-    
+
     if (status == ST_ERROR_IF_NO_INGRESADO)
         fprintf(stderr, "%s. %s\n", MSJ_ERROR_IF_NO_INGRESADO, MSJ_MAS_AYUDA);
+
+    if (status == ST_ERROR_OF_NO_VALIDO)
+        fprintf(stderr, "%s. %s\n", MSJ_ERROR_OF_NO_VALIDO, MSJ_MAS_AYUDA);
+
+    if (status == ST_ERROR_OF_NO_INGRESADO)
+        fprintf(stderr, "%s. %s\n", MSJ_ERROR_OF_NO_INGRESADO, MSJ_MAS_AYUDA);
 }
