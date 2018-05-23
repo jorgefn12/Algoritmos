3f7146d5bbd3e3c0fefb11f4aaf35d090c878f0c
jorgefn12 <37821421+jorgefn12@users.noreply.github.com>
2018-05-23 17:35:04 -0300

    Update dump.c

--- TP1- Simpletron/dump.c
+++ TP1- Simpletron/dump.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #include "header.h"
 
+/*
+Esta función se encarga de seleccionar dónde y de qué forma se debe imprimir el volcado del
+programa "dump" y delega dichas tareas a otras funciones.
+Recibe como argumentos un archivo_t que indica el formato de salida, una cadena con el nombre
+del archivo y una copia de la estructura del simpletron.
+Retorna por el nombre el estado de la función.
+*/
 status_t dump(archivo_t tipo_archivo_salida, char* nombre_archivo_salida, palabras_s palabra) {
     status_t status = ST_OK;
 
@@ -28,6 +35,13 @@ status_t dump(archivo_t tipo_archivo_salida, char* nombre_archivo_salida, palabr
     return status;
 }
 
+/*
+Esta función imprime la memoria del simpletron junto con los últimos registros utilizados, en un
+archivo o por stdout con formato de texto.
+La función recibe una copia de estructura (simpletron), una cadena con el nombre del archivo de
+salida, y un archivo_t con el formato de salida (texto).
+Retorna por el nombre el estado de la función.
+*/
 status_t imprimir_dump_por_stdout_o_txt(palabras_s palabra, char *nombre_archivo_salida, archivo_t tipo_archivo_salida) {
     int i, j;
     FILE *archivo_salida;
@@ -69,6 +83,13 @@ status_t imprimir_dump_por_stdout_o_txt(palabras_s palabra, char *nombre_archivo
     return ST_OK;
 }
 
+/*
+Esta función imprime la memoria del simpletron junto con los últimos registros utilizados en un
+archivo con formato binario.
+La función recibe una copia de estructura (simpletron), una cadena con el nombre del archivo de
+salida, y un archivo_t con el formato de salida (binario).
+Retorna por el nombre el estado de la función.
+*/
 status_t imprimir_dump_bin(palabras_s palabra, char *nombre_archivo_salida, archivo_t tipo_archivo_salida) {
     int i;
     FILE *archivo_salida;
