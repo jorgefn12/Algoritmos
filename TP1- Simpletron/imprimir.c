ad08ab15be18948f05010769dbacc53ec9b3d4ee
jorgefn12 <37821421+jorgefn12@users.noreply.github.com>
2018-05-23 16:44:54 -0300

    Update imprimir.c

--- TP1- Simpletron/imprimir.c
+++ TP1- Simpletron/imprimir.c
@@ -58,6 +58,9 @@ void imprimir_errores(status_t status) {
         case ST_ERROR_ESCRIBIR_BIN:
             fprintf(stderr, "%s. %s\n", MSJ_ERROR_ESCRIBIR_BIN, MSJ_MAS_AYUDA);
             break;
+        case ST_ERROR_MAX_INGRESOS_SUPERADO:
+		    fprintf(stderr, "%s. %s\n", MSJ_ERROR_MAX_INGRESOS_SUPERADO, MSJ_MAS_AYUDA);
+	       	break;
         default:
             fprintf(stderr, "%s. %s\n", MSJ_ERROR, MSJ_MAS_AYUDA);
     }
