69695111c9f845a57f401618c3e36b043f9ec0e3
Karen <31966611+oonosan@users.noreply.github.com>
2018-06-16 18:10:39 -0300

    Create validaciones.h

---
+++ TP2/validaciones.h
@@ -0,0 +1,14 @@
+#define CANT_MIN_ARG 2 
+/*Para el correcto funcionamiento del programa, la menor cantidad de argumentos ingresados
+ es el caso de ./simpletron --help*/
+
+/*FLAGS CLA*/
+#define FLAG_CLA_AYUDA_CORTO "-h"
+#define FLAG_CLA_AYUDA_LARGO "--help"
+#define FLAG_CLA_MEMORIA_CORTO "-m"
+#define FLAG_CLA_MEMORIA_LARGO "--memoria"
+#define FLAG_CLA_FORMATO_CORTO "-f"
+#define FLAG_CLA_FORMATO_LARGO "--formato"
+
+
+status_t validacion_cla(int argc, char** argv);
