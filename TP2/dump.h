9d4dbbaf0eba7ab5bf7b18d67e7f78b28a5c910f
jorgefn12 <37821421+jorgefn12@users.noreply.github.com>
2018-07-06 19:03:57 -0300

    Create dump.h

---
+++ TP2/dump.h
@@ -0,0 +1,16 @@
+#ifndef DUMP__H
+#define DUMP__H 1
+
+#define DUMP_MSJ_REGISTROS "\nREGISTROS:"
+#define DUMP_MSJ_ACUMULADOR "acumulador"
+#define DUMP_MSJ_PROGRAM_COUNTER "program counter"
+#define DUMP_MSJ_INSTRUCCION "instruccion"
+#define DUMP_MSJ_OPCODE "opcodo"
+#define DUMP_MSJ_OPERANDO "operando"
+#define DUMP_MSJ_MEMORIA "\nMEMORIA"
+
+status_t imprimir_dump(simpletron_s * simpletron, params_s * param);
+status_t imprimir_dump_txt(simpletron_s* simpletron, params_s * param);
+status_t imprimir_dump_bin(simpletron_s* simpletron, params_s * param);
+
+#endif
