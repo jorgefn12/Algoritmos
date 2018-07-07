c21e0eaba65c9a196954ec6a46e791f3ca171eb5
Karen <31966611+oonosan@users.noreply.github.com>
2018-07-06 23:40:05 -0300

    Update comun.h

--- TP2/comun.h
+++ TP2/comun.h
@@ -1,25 +1,10 @@
-/*typedef de los operandos*/
-typedef enum {
-    OP_LEER = 10,
-    OP_ESCRIBIR = 11
-} opcode_t;
-
-typedef enum {
-    FALSE = 0,
-    TRUE
-} bool_t;
-
-typedef enum {
-    ST_OK,
-    ST_HELP,
-    ST_ERROR_PTR_NULO,
-    ST_ERROR_ARCHIVO_NO_ENCONTRADO,
-    ST_ERROR_MEMORIA,
-    /*Validacion cla*/
-    ST_ERROR_CANT_ARG,
-    ST_ERROR_FLAG_NO_RECONOCIDO,
-    ST_ERROR_MEMORIA_INGRESADA_INVALIDA,
-    ST_ERROR_FORMATO_ARCHIVO_INVALIDO,
-            ST_ERROR_STDIN_INVALIDO
-} status_t;
-
+#define MAX_STR 200
+#define BUF_SIZE_BIN 16
+#define MAX_PALABRA 9999999
+#define MIN_PALABRA -9999999
+#define STR_FIN_CARGA "-99999999"
+#define OPCODE_OPERANDO 10000
+#define OPCODE_MIN -64
+#define OPCODE_MAX 64
+#define OPERANDO_MIN 0
+#define OPERANDO_MAX 512
