d79cc76dbccb4b6a9ba87de13f44480d228486be
Karen <31966611+oonosan@users.noreply.github.com>
2018-07-07 14:18:52 -0300

    Create carga.h

---
+++ TP2/carga.h
@@ -0,0 +1,12 @@
+#ifndef CARGA_H
+#define CARGA_H
+
+#define BUF_SIZE_BYTE 8
+#define BYTE_OPCODE 0x7F00
+#define BYTE_OPCODE_SHIFT 7
+#define BYTE_OPERANDO 0xFF
+#define BYTE_OPERANDO_SHIFT 0
+#define BYTE_SHIFT 8
+
+#endif /* CARGA_H */
+
