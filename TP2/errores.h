0b2a126812dcd0190da83c15af81882966e37709
Karen <31966611+oonosan@users.noreply.github.com>
2018-07-06 23:41:40 -0300

    Update errores.h

--- TP2/errores.h
+++ TP2/errores.h
@@ -1,3 +1,13 @@
 #define MSJ_OPCION_AYUDA "Ingrese: .\\simpletron -h o .\\simpletron --help para mas informacion.\n"
+#define MSJ_ST_ERROR_ARCHIVO_NO_ENCONTRADO "El archivo ingresado no se encuentra"
+#define MSJ_ST_ERROR_PTR_NULO "Puntero nulo"
+#define MSJ_ST_ERROR_MEMORIA "Ocurrio un error al pedir memoria"
+/*validaciones*/
+#define MSJ_ST_ERROR_MEMORIA_INGRESADA_INVALIDA "La memoria ingresada no es valida, la misma debe ser un entero positivo"
+#define MSJ_ST_ERROR_FORMATO_ARCHIVO_INVALIDO "El formato de archivo de salida no es valido"
+#define MSJ_ST_ERROR_STDIN_INVALIDO "stdin no valido"
 #define MSJ_ST_ERROR_CANT_ARG "La cantidad de argumentos ingresados por la terminal no es valido."
-#define MSJ_ST_ERROR_FLAG_NO_RECONOCIDO "El flag ingresado no es valido."
+/*carga.c*/
+#define MSJ_ST_ERROR_CREAR_SIMPLETRON "Ocurrio un error al crear e inicializar el simpletron"
+#define MSJ_ST_ERROR_LEER_PALABRA "Ocurrio un error al leer palabra del archivo"
+#define MSJ_ST_ERROR_PALABRA_NO_VALIDA "La palabra ingresada no es valida"
