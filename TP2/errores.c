c49ea27c4bea1afc48b6d92983c9be787edfa9e8
Karen <31966611+oonosan@users.noreply.github.com>
2018-07-07 15:45:10 -0300

    Update errores.c

--- TP2/errores.c
+++ TP2/errores.c
@@ -40,6 +40,40 @@ void imprimir_mensaje_de_error(status_t status) {
         case ST_ERROR_PALABRA_NO_VALIDA:
             fprintf(stderr, "%s. %s\n", MSJ_ST_ERROR_PALABRA_NO_VALIDA, MSJ_OPCION_AYUDA);
             break;
+            
+            /*Ejecutar codigo*/
+            case ST_HALT:
+            fprintf(stderr, "%s. %s\n", MSJ_ST_HALT, MSJ_OPCION_AYUDA);
+            break;
+            case ST_ERROR_CAD_NO_ES_ENTERO:
+            fprintf(stderr, "%s. %s\n", MSJ_ST_ERROR_CAD_NO_ES_ENTERO, MSJ_OPCION_AYUDA);
+            break;
+            case ST_ERROR_PALABRA_FUERA_DE_RANGO:
+            fprintf(stderr, "%s. %s\n", MSJ_ST_ERROR_PALABRA_FUERA_DE_RANGO, MSJ_OPCION_AYUDA);
+            break;
+            case ST_ERROR_SEGMENTATION_FAULT:
+            fprintf(stderr, "%s. %s\n", MSJ_ST_ERROR_SEGMENTATION_FAULT, MSJ_OPCION_AYUDA);
+            break;
+            case ST_ERROR_CAD_NO_LEIDA:
+            fprintf(stderr, "%s. %s\n", MSJ_ST_ERROR_CAD_NO_LEIDA, MSJ_OPCION_AYUDA);
+            break;
+            case ST_ERROR_OPCODE_INVALIDO:
+            fprintf(stderr, "%s. %s\n", MSJ_ST_ERROR_OPCODE_INVALIDO, MSJ_OPCION_AYUDA);
+            break;
+            case ST_ERROR_MAX_INSTR_SUPERADO:
+            fprintf(stderr, "%s. %s\n", MSJ_ST_ERROR_MAX_INSTR_SUPERADO, MSJ_OPCION_AYUDA);
+            break;
+            case ST_ERROR_MAX_INGRESOS_SUPERADO:
+            fprintf(stderr, "%s. %s\n", MSJ_ST_ERROR_MAX_INSTR_SUPERADO, MSJ_OPCION_AYUDA);
+            break;
+        case ST_ERROR_NODO_VACIO:
+            fprintf(stderr, "%s. %s\n", MSJ_ST_ERROR_NODO_VACIO, MSJ_OPCION_AYUDA);
+            break;
+            
+            /*Mensajes de error de dump.c*/
+        case ST_ERROR_ESCRIBIR_BIN:
+            fprintf(stderr, "%s. %s\n", MSJ_ST_ERROR_ESCRIBIR_BIN, MSJ_OPCION_AYUDA);
+            break;
         default:
             fprintf(stderr, "%s\n", MSJ_OPCION_AYUDA);
     }
