741c9e8d3aabff338a409adb536f56ca6a542424
jorgefn12 <37821421+jorgefn12@users.noreply.github.com>
2018-07-08 00:27:17 -0300

    Update carga.h

--- TP2/carga.h
+++ TP2/carga.h
@@ -10,3 +10,29 @@
 #define BYTE_MSB 0x8000
 #endif /* CARGA_H */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include "tipos.h"
+#include "comun.h"
+#include "carga.h"
+
+archivo_s *crear_archivo(void) ;
+simpletron_s *crear_simpletron(void) ;
+status_t crear_nodo_simpletron(simpletron_s **simpletron, void *dato) ;
+status_t crear_lista_memoria(palabra_s **p) ;
+status_t crear_lista_memoria_nodo(palabra_s **nodo, int dato) ;
+status_t cargar_simpletron(simpletron_s ** frente, params_s params) ;
+status_t cargar_archivo(archivo_s *archivo, char *nombre_archivo, formato_t fmt, FILE *f);
+status_t cargar_palabras_bin(FILE *archivo_entrada, archivo_s *archivo) ;
+
+status_t cargar_palabras_txt(FILE *f, archivo_s *archivo);
+status_t cargar_palabra_stdin(archivo_s *archivo, params_s params);
+status_t palabra_valida(long palabra);
+status_t palabra_valida_bin(uint16_t palabra);
+status_t insertar_en_simpletron(simpletron_s **simpletron, archivo_s *archivo);
+status_t insertar_palabra_al_principio_de_lista(palabra_s **p, int d);
+status_t insertar_palabra_al_final_de_lista(palabra_s **frente, long dato);
+char* strdup(const char *sc);
+void imprimir_palabras_de_un_archivo(simpletron_s *simpletron);
