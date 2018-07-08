eeea44ef6d1ce2217ff968fd99d471ebcbc2f6bf
jorgefn12 <37821421+jorgefn12@users.noreply.github.com>
2018-07-08 00:24:07 -0300

    Update dump.c

--- TP2/dump.c
+++ TP2/dump.c
@@ -1,17 +1,15 @@
 
-/***************************************DUMP.C***********************************************************/
-/*Para poder imprimir los registros en hexadecimal, tuve que cambiar el tipo de palabra_t por un unsigned int*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "dump.h"
-#include "ejecutar_codigo.h" /*Necesita una función para recorrer memorias simpletron que tengo implementado en ejecutar_codigo*/
+#include "ejecutar_codigo.h"
 #include "comun.h"
 #include "tipos.h"
 
-/*
-*Luego de ejecutar todos los simpletron, dump imprime los registros de cada simpletron por separado dentro
-*de un unico archivo comun: "dump"
+/*Esta función recibe un puntero a estructura simpletron, e imprime sus registros y memoria de forma independiente en un mismo
+*archivo dump. Dependiendo del tipo de formato de salida, esta función delega el procesamiento por texto o binario a otras
+*funciones. Al finalizar retorna el estado de la función.
 */
 
 status_t imprimir_dump(simpletron_s * simpletron, params_s * param){
@@ -19,7 +17,7 @@ status_t imprimir_dump(simpletron_s * simpletron, params_s * param){
     
     switch(param->archivo_salida.formato){
         case(FMT_TXT):
-            while(simpletron){
+            while(simpletron->sig){
                 status = imprimir_dump_txt(simpletron, param);
                 if(status != ST_OK)
                     return status;
@@ -27,7 +25,7 @@ status_t imprimir_dump(simpletron_s * simpletron, params_s * param){
             }
             break;
         case(FMT_BIN):
-            while(simpletron){
+            while(simpletron->sig){
                 status = imprimir_dump_bin(simpletron, param);
                 if(status != ST_OK)
                     return status;
@@ -49,16 +47,16 @@ status_t imprimir_dump_txt(simpletron_s* simpletron, params_s * param){
     fprintf(archivo_salida, "%15s:    %02ld\n", DUMP_MSJ_PROGRAM_COUNTER, simpletron->program_counter);
     fprintf(archivo_salida, "%15s: %+5d\n", DUMP_MSJ_INSTRUCCION, simpletron->instruccion);
     fprintf(archivo_salida, "%15s:    %02d\n", DUMP_MSJ_OPCODE, simpletron->opcode);
-    fprintf(archivo_salida, "%15s:    %02d\n", DUMP_MSJ_OPERANDO, simpletron->operando);
+    fprintf(archivo_salida, "%15s:    %03d\n", DUMP_MSJ_OPERANDO, simpletron->operando);
     fprintf(archivo_salida, "%s:\n", DUMP_MSJ_MEMORIA);
 
-    for (i = 0, j = 0; i < simpletron->archivo->cant_palabras; i++) {
+    for (i = 0, j = 0; simpletron->archivo->memoria != NULL; i++) {
         if (!(i % 10))
             fprintf(archivo_salida, "\n%3ld:", j++ * 10);
         
-        fprintf(archivo_salida, " %+05d", avanzar_lista_n (simpletron->archivo->memoria, i)->dato);
+        fprintf(archivo_salida, " %+05d", simpletron->archivo->memoria->dato);
+        simpletron->archivo->memoria = avanzar_lista_n (simpletron->archivo->memoria, 1);
     }
-    puts("\n");
     fclose(archivo_salida);
     return ST_OK;
 }
@@ -69,7 +67,7 @@ status_t imprimir_dump_bin(simpletron_s* simpletron, params_s * param){
     if ((archivo_salida = fopen(param->archivo_salida.nombre, "a")) == NULL)
         return ST_ERROR_ARCHIVO_NO_ENCONTRADO;
 
-    for (i = 0; i < simpletron->archivo->cant_palabras; i++) {
+    for (i = 0; simpletron->archivo->memoria != NULL; i++) {
         if ((fwrite(&(avanzar_lista_n (simpletron->archivo->memoria, i)->dato), sizeof (int), 1, archivo_salida)) != 1)
             return ST_ERROR_ESCRIBIR_BIN;
     }
