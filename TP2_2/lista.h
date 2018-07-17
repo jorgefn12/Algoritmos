d419dea3a0613d2878ee283b8dfa5b8ae9e79e44
jorgefn12 <37821421+jorgefn12@users.noreply.github.com>
2018-07-17 00:44:15 -0300

    Update lista.h

--- TP2_2/lista.h
+++ TP2_2/lista.h
@@ -1,1 +1,11 @@
 /*Prototipos de TDA Lista*/
+#ifndef LISTA__H
+#define LISTA__H
+
+typedef struct nodo nodo_t, * lista_t; /*Aviso la existencia de los tipos nodo_t y lista_t (puntero a nodo_t)
+/*Primitivas*/
+
+
+
+
+#endif
