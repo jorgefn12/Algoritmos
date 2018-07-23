6845c7fc2e35d5c5fab28155924a768d66a51120
jorgefn12 <37821421+jorgefn12@users.noreply.github.com>
2018-07-23 11:16:55 -0300

    Update lista.h

--- TP2_2/lista.h
+++ TP2_2/lista.h
@@ -2,10 +2,17 @@
 #ifndef LISTA__H
 #define LISTA__H
 
-typedef struct nodo nodo_t, * lista_t; /*Aviso la existencia de los tipos nodo_t y lista_t (puntero a nodo_t)
-/*Primitivas*/
+typedef struct nodo{
+    struct nodo * sig;
+    void * dato;
+} nodo_t, * lista_t; /********ENCAPSULAR luego de probar funcionamiento del programa*************/
 
-
-
-
+status_t crear_lista(lista_t * lista);
+nodo_t * crear_nodo(void * dato);
+bool_t lista_esta_vacia(lista_t lista);
+status_t insertar_nodo_final(lista_t * lista, void * dato);
+status_t insertar_nodo_ppio(lista_t * lista, void * dato);
+void destruir_lista(lista_t * lista);
+void imprimir_lista_int(lista_t lista);
+bool_t guardar_lista_en_vector(lista_t lista, vector_t * vector);
 #endif
