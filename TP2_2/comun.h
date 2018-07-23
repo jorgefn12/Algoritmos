d040723156e4a80708bfcbd474c6de65c9f8c4ec
jorgefn12 <37821421+jorgefn12@users.noreply.github.com>
2018-07-23 11:05:14 -0300

    Update comun.h

--- TP2_2/comun.h
+++ TP2_2/comun.h
@@ -2,18 +2,5 @@
 #define COMUN__H
 
 #define MAX_STR 200
-#define BUF_SIZE_BIN 16
-#define MAX_PALABRA 9999999
-#define MIN_PALABRA -9999999
-#define STR_FIN_CARGA "-99999999"
-#define OPCODE_OPERANDO 10000
-/*
-#define OPCODE_MIN -64
-#define OPCODE_MAX 64
-*/
-#define OPCODE_MIN 0
-#define OPCODE_MAX 127
-#define OPERANDO_MIN 0
-#define OPERANDO_MAX 511
 
 #endif
