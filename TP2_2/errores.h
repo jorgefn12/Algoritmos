f4848fa44c4633b9268e94e3634c7cfb264d743b
jorgefn12 <37821421+jorgefn12@users.noreply.github.com>
2018-07-23 11:09:25 -0300

    Update errores.h

--- TP2_2/errores.h
+++ TP2_2/errores.h
@@ -1,29 +1,17 @@
 #ifndef ERRORES__H
 #define ERRORES__H
 
-#define MSJ_OPCION_AYUDA "Ingrese: .\\simpletron -h o .\\simpletron --help para mas informacion.\n"
-#define MSJ_ST_ERROR_ARCHIVO_NO_ENCONTRADO "El archivo ingresado no se encuentra"
+#define MSJ_OPCION_AYUDA "Ingrese: .\\simpletron -h o .\\simpletron --help para mas informacion."
+#define MSJ_ST_OK "La operación finalizó correctamente"
+#define MSJ_ST_AYUDA "Leer documentación. Escribir función para mensaje de ayuda"
 #define MSJ_ST_ERROR_PTR_NULO "Puntero nulo"
 #define MSJ_ST_ERROR_MEMORIA "Ocurrio un error al pedir memoria"
-/*validaciones*/
+#define MSJ_ST_ERROR_ARCHIVO_NO_ENCONTRADO "El archivo ingresado no se encuentra"
+/*Validacion de argumentos*/
 #define MSJ_ST_ERROR_MEMORIA_INGRESADA_INVALIDA "La memoria ingresada no es valida, la misma debe ser un entero positivo"
 #define MSJ_ST_ERROR_FORMATO_ARCHIVO_INVALIDO "El formato de archivo de salida no es valido"
 #define MSJ_ST_ERROR_STDIN_INVALIDO "stdin no valido"
-#define MSJ_ST_ERROR_CANT_ARG "La cantidad de argumentos ingresados por la terminal no es valido."
-/*carga.c*/
-#define MSJ_ST_ERROR_CREAR_SIMPLETRON "Ocurrio un error al crear e inicializar el simpletron"
-#define MSJ_ST_ERROR_LEER_PALABRA "Ocurrio un error al leer palabra del archivo"
-#define MSJ_ST_ERROR_PALABRA_NO_VALIDA "La palabra ingresada no es valida"
-/*ejecutar_codigo*/
-#define MSJ_ST_HALT "Ejecución de archivo finalizado"
-#define MSJ_ST_ERROR_SEGMENTATION_FAULT "Se trató de ingresar a memoria no pedida por la consola"
-#define MSJ_ST_ERROR_OPCODE_INVALIDO "Se trató de ejecutar una instrucción no espeficada por el lenguaje"
-#define MSJ_ST_ERROR_MAX_INSTR_SUPERADO "Se superó la máxima cantidad de instrucciones operadas"
-#define MSJ_ST_ERROR_MAX_INGRESOS_SUPERADO "Se superó la máxima cantidad de intentos de ingreso"
-#define MSJ_ST_ERROR_NODO_VACIO "Se intenta acceder a memoria sin contenido"
-#define MSJ_ST_ERROR_ESCRIBIR_BIN "Ocurrio un errror al esribir el archivo binario"
-/*
-void imprimir_ayuda() {
-}
-*/
+void imprimir_estado(status_t status);
+void imprimir_ayuda(void);
+
 #endif
