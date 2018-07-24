14fcb17a20273c4bd073ec99e47015e831db1f2d
jorgefn12 <37821421+jorgefn12@users.noreply.github.com>
2018-07-24 09:36:13 -0300

    Update lista.h

--- TP2_2/lista.h
+++ TP2_2/lista.h
@@ -1,18 +1,10 @@
-/*Prototipos de TDA Lista*/
-#ifndef LISTA__H
-#define LISTA__H
+#ifndef LISTAS__H
+#define LISTAS__H
 
 #include "tipos.h"
-#include "comun.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "memoria.h"
 
-typedef struct nodo{
-    struct nodo * sig;
-    void * dato;
-} nodo_t, * lista_t; /********ENCAPSULAR luego de probar funcionamiento del programa*************/
-
+typedef struct nodo nodo_t, * lista_t;
 status_t crear_lista(lista_t * lista);
 nodo_t * crear_nodo(void * dato);
 bool_t lista_esta_vacia(lista_t lista);
@@ -20,5 +12,6 @@ status_t insertar_nodo_final(lista_t * lista, void * dato);
 status_t insertar_nodo_ppio(lista_t * lista, void * dato);
 void destruir_lista(lista_t * lista);
 void imprimir_lista_int(lista_t lista);
-bool_t guardar_lista_en_vector(lista_t lista, vector_t * vector);
+bool_t guardar_lista_en_vector(lista_t lista, vector_t ** vector);
+
 #endif
