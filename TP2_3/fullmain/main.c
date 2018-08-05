b3a5aa31384068567e3d7b77022d907fd2e9901a
jorgefn12 <37821421+jorgefn12@users.noreply.github.com>
2018-08-05 16:53:14 -0300

    Update main.c

--- TP2_3/fullmain/main.c
+++ TP2_3/fullmain/main.c
@@ -247,28 +247,6 @@ palabra_t invertir_complemento_a_2(palabra_t palabra, palabra_t BIT_FIELD);
 void imprimir_palabra_txt(palabra_t palabra, formato_t formato);
 palabra_t cambiar_fmt_palabra(palabra_t palabra, formato_t destino);
 
-
-int main(int argc, char** argv){
-    status_t st;
-    params_t argumentos;
-    size_t i, j;
-    simpletron_t * simply;
-    lista_t lista = NULL;
-    
-    validacion_cla(argc,argv, &argumentos);
-    for(i = 0; i < argumentos.cant_archivos; i++){
-        simply = SIMPLETRON_crear();
-        leer_archivo(&simply->memoria, &argumentos.archivo_entrada[i], argumentos.cant_memoria);
-        LISTA_insertar_al_final(&lista,simply);
-    }
-    LISTA_aplicar(lista, SIMPLETRON_ejecutar);
-    LISTA_recorrer(lista, imprimir_dump, argumentos.archivo_salida);
-    LISTA_destruir(&lista, SIMPLETRON_destruir);
-    
-    return EXIT_SUCCESS;
-}
-
-
 int main(int argc, char** argv) {
     
     /*MAIN PARA TESTEAR PRIMITIVAS DE LISTAS
