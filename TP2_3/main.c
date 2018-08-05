8d91e7dba684cdc2b0634f71dff1975eedba94d4
jorgefn12 <37821421+jorgefn12@users.noreply.github.com>
2018-08-05 16:42:06 -0300

    Update main.c

--- TP2_3/main.c
+++ TP2_3/main.c
@@ -1,1 +1,21 @@
 
+
+int main(int argc, char** argv){
+    status_t st;
+    params_t argumentos;
+    size_t i, j;
+    simpletron_t * simply;
+    lista_t lista = NULL;
+    
+    validacion_cla(argc,argv, &argumentos);
+    for(i = 0; i < argumentos.cant_archivos; i++){
+        simply = SIMPLETRON_crear();
+        leer_archivo(&simply->memoria, &argumentos.archivo_entrada[i], argumentos.cant_memoria);
+        LISTA_insertar_al_final(&lista,simply);
+    }
+    LISTA_aplicar(lista, SIMPLETRON_ejecutar);
+    LISTA_recorrer(lista, imprimir_dump, argumentos.archivo_salida);
+    LISTA_destruir(&lista, SIMPLETRON_destruir);
+    
+    return EXIT_SUCCESS;
+}
