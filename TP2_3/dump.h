3c79ae07f5463a8eb03f55a8ed32585c612b7f20
jorgefn12 <37821421+jorgefn12@users.noreply.github.com>
2018-08-06 09:11:17 -0300

    Update dump.h

--- TP2_3/dump.h
+++ TP2_3/dump.h
@@ -1,1 +1,14 @@
+#ifndef DUMP__H
+#define DUMP__H
 
+#include "simpletron.h"
+#include "idiomas.h"
+
+#define MASK_LOW_BYTE 0xFF
+#define MASK_HIGH_BYTE 0xFF00
+
+void imprimir_dump(simpletron_t * simpletron, archivo_t * archivo_salida);
+void imprimir_dump_txt(simpletron_t * simpletron, FILE * stream);
+void imprimir_dump_bin(simpletron_t * simpletron, FILE * stream);
+
+#endif
