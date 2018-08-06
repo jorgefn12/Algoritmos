6640ff35d7a7dbd574cdc304cc208c543b36d22d
jorgefn12 <37821421+jorgefn12@users.noreply.github.com>
2018-08-06 09:12:43 -0300

    Update idiomas.h

--- TP2_3/idiomas.h
+++ TP2_3/idiomas.h
@@ -1,1 +1,17 @@
 
+#ifndef IDIOMAS__H
+#define IDIOMAS__H
+
+#include "setup.h"
+
+#ifdef LENGUAJE_ESP 
+#include "dump_esp.h"
+#include "errores_esp.h"
+#include "simpletron_esp.h"
+
+#elif defined LENGUAJE ING
+/*No implementado*/
+#endif
+
+#endif /* IDIOMAS_H */
+
